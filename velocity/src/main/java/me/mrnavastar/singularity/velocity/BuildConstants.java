969ffff3a3fd94bd1384ba27077c757a47067916
MrNavaStar <Mr.NavaStar@gmail.com>
2023-10-24 11:17:39 -0600

    Remove velocity - redundant to client

--- velocity/src/main/java/me/mrnavastar/singularity/velocity/BuildConstants.java
+++
@@ -1,9 +0,0 @@
-package me.mrnavastar.singularity.velocity;
-
-// The constants are replaced before compilation
-public class BuildConstants {
-
-    public static final String MOD_ID = "singularity";
-    public static final String MOD_NAME = "Singularity";
-    public static final String VERSION = "${version}";
-}
