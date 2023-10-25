204015a59dca2576d19b461e1fcb93c1917bd025
MrNavaStar <Mr.NavaStar@gmail.com>
2023-10-25 00:41:16 -0600

    clean

--- core/src/main/java/me/mrnavastar/protoweaver/protocol/protoweaver/ProtoWeaver.java
+++ core/src/main/java/me/mrnavastar/protoweaver/protocol/protoweaver/ProtoWeaver.java
@@ -11,14 +11,13 @@ import java.util.HashMap;
 
 public class ProtoWeaver implements ProtoPacketHandler {
 
+    private static final HashMap<String, Protocol> loadedProtocols = new HashMap<>();
     @Getter
     private static final Protocol protocol = ProtoBuilder.protocol("protoweaver", "internal")
             .setServerHandler(ProtoWeaver.class)
             .setClientHandler(ProtoWeaver.class)
             .addPacket(Handshake.class)
             .build();
-
-    private static final HashMap<String, Protocol> loadedProtocols = new HashMap<>();
 
     static {
         load(protocol);
@@ -36,10 +35,7 @@ public class ProtoWeaver implements ProtoPacketHandler {
         }
 
         // Only respond if handshake from client
-        if (handshake.from(Handshake.Side.CLIENT)) {
-            connection.send(new Handshake(handshake.getProtocolName(), Handshake.Side.SERVER));
-        }
-
+        if (handshake.from(Handshake.Side.CLIENT)) connection.send(new Handshake(handshake.getProtocolName(), Handshake.Side.SERVER));
         connection.upgradeProtocol(newProtocol, newProtocol.newServerHandler());
     }
 
