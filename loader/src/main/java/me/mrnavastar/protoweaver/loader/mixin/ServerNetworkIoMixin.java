ea047d4625007a041035227eb30c5031af512f74
MrNavaStar <Mr.NavaStar@gmail.com>
2023-11-06 02:40:29 -0700

    move mixin inject point to be more compatible across game versions

---
+++ loader/src/main/java/me/mrnavastar/protoweaver/loader/mixin/ServerNetworkIoMixin.java
@@ -0,0 +1,17 @@
+package me.mrnavastar.protoweaver.loader.mixin;
+
+import io.netty.channel.Channel;
+import me.mrnavastar.protoweaver.loader.netty.ProtoDeterminer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net/minecraft/server/ServerNetworkIo$1")
+public class ServerNetworkIoMixin {
+
+    @Inject(method = "initChannel", at = @At("HEAD"))
+    public void bind(Channel channel, CallbackInfo ci) {
+        channel.pipeline().addLast("protoDeterminer", new ProtoDeterminer());
+    }
+}
